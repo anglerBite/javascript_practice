d0caa57de601439f1fb26eb822e11221a5eb1dde
anglerBite <jackall489@gmail.com>
2023-09-14 01:34:28 +0900

    first commit

---
+++ hidouki.js
@@ -0,0 +1,28 @@
+"use strict";
+// function sleep(val: any){
+//     return new Promise(function(resolve){
+//         setTimeout(function(){
+//             console.log(val++);
+//             resolve(val);
+//         },1000)
+//     });
+// }
+// sleep(0)
+// async function init() {
+//     await sleep(0);
+// }
+// init();
+// interface Human {
+//     name: string;
+//     age: number;
+// }
+// const Human: Human = {
+//     name: 'Quill',
+//     age: 38,
+// }
+// console.log(Human);
+console.log('こんにちは');
+setTimeout(() => {
+    console.log('こんばんは');
+}, 3000);
+console.log('私が先だ');
